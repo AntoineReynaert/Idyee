a2d7607ba2a812751904c3415eee7f147cf571ec
Tonio <tonio@linux.home>
2020-11-23 22:03:17 +0100

    filtrage des aides

--- read_aides.py
+++ read_aides.py
@@ -2,11 +2,6 @@
 
 import csv
 
-"""
- Fonction permettant de lire un fichier CSV et de récupérer les données dans une liste.
- Arguments: 
-   - str file: nom d'un fichier csv dans le répertoire
-"""
 def openCsv(file):
     with open(file, newline='' , encoding="utf8", errors= "ignore") as csvfile:
         spamreader = csv.reader(csvfile, delimiter=';')
@@ -15,27 +10,39 @@ def openCsv(file):
             donnees.append(row)
     return donnees
 
+def writeCsv(file, donnees):
+    with open(file, 'w', newline='' , encoding="utf8", errors= "ignore") as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=';')
+        for row in donnees:
+            spamwriter.writerow(row)
+
+def filter_all(donnees):
+    # ici on regarde la présence du mot véhicule dans au moins un des colonnes
+    donnees_filtrees = [donnees[0]] #Ajout de l'entête
+    for ligne in donnees:
+        for elem in ligne:
+            count = 0 # pour éviter de récupérer plusieurs fois la même ligne si il y a plusieurs fois le mot vehicule
+            if "hicul" in elem and count ==0:
+                donnees_filtrees.append(ligne)
+                count += 1
+    return donnees_filtrees
+
+def filter_name(donnees):
+    # ici on regarde la présence du mot véhicule uniquement dans la colonne aid_nom
+    donnees_filtrees = [donnees[0]] #Ajout de l'entête
+    for ligne in donnees:
+        count=0
+        if "hicul" in ligne[1] and count ==0:
+            donnees_filtrees.append(ligne)
+            count += 1
+    return donnees_filtrees
+
 def main():
-	donnes = openCsv("aides.csv")
-	donnes_filtrees=list()
-	donnes_filtrees.append(donnes[0]) #Ajout de l'entête
-	for ligne in donnes[:10]:
-		for elem in ligne:
-			count = 0
-			if "hicul" in elem and count ==0:
-				print("\r",ligne)
-				donnes_filtrees.append(ligne)
-
-	print("\r\r")
-
-	with open('aides_filtrees.csv', 'w', newline='') as csvfile:
-
-	    spamwriter = csv.writer(csvfile, delimiter=' ',
-	                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
-	    for row in donnes_filtrees:
-	    	print("\r", donnes_filtrees)
-	    	# spamwriter.writerow(donnes_filtrees)
+    donnees = openCsv("aides.csv")
+    filtered_all = filter_all(donnees) 
+    filtered_name = filter_name(donnees)
+    writeCsv('aides_filtrees_toutes_les_colonnes.csv', filtered_all)
+    writeCsv('aides_filtrees_nom.csv', filtered_name)
 
 
 if __name__ == "__main__":
