ef2b7283045f02f1964e8de96967da732d73a705
Khalid Jeffal <khalid.jeffal@grenoble-inp.org>
2020-11-23 19:15:41 +0100

    Replace coord_gps.py

--- coord_gps.py
+++ coord_gps.py
@@ -9,11 +9,24 @@ def get_coord(adresse):
     api_url = "https://api-adresse.data.gouv.fr/search/?q="
     r = requests.get(api_url + urllib.parse.quote(adresse)).json()
     if not r["features"]:
-        print("Nous n'arrivons pas à trouver l'adresse que vous avez renseignée")
-        print("Veuillez saisir une adresse correcte de la forme : Numéro,Nom de rue,Code Postal,Ville")
+        return []
     else:
 
         return (r["features"][0]["geometry"]["coordinates"])
+def get_adr(coord):
+    api_url="https://api-adresse.data.gouv.fr/reverse/?"
+    query='lon='+str(coord[0])+'&lat='+str(coord[1])
+    
+    r = requests.get(api_url + query).json()
+    return r["features"][0]["properties"]["label"]
 
+def get_city(coord):
+    api_url="https://api-adresse.data.gouv.fr/reverse/?"
+    query='lon='+str(coord[0])+'&lat='+str(coord[1])
+    r = requests.get(api_url + query).json()
+    if not r["features"]:
+        return ""
+    else:
+        return r["features"][0]["properties"]["postcode"]
 
 
