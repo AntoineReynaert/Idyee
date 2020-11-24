a6abe01edeea0c7a26a42a37d4134e6af2a3e0d8
Tonio <tonio@linux.home>
2020-11-24 18:11:42 +0100

    add execercice

---
+++ exercice.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json
+from read_aides import openCsv
+
+def openJson(file):
+	with open(file) as json_data:
+	    data_dict = json.load(json_data)
+	    return data_dict
+
+def afficher_dico(dic):
+	for titre_donnee, donnee in dic.items():
+		print(" {:30} {} ".format(titre_donnee,donnee))
+
+def main():
+	fichier='donnees_client_example.json'
+	donnees_client = openJson(fichier)
+
+	afficher_dico(donnees_client)
+
+#1  Afficher la capcite d'investissment la societe
+#2 Afficher le nombre de voitures thermiques
+#3 Calculer le nombre de voitures electriques que peut acheter l'entrerpise avec sa capacite d'investissment si une voiture electrique coute 10 000€ 
+
+if __name__ == "__main__":
+    main()
