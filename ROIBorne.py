9ed94bab4827986e8f3343db2fca783447c3466f
unknown <alexandre.galas@grenoble-inp.fr>
2020-12-13 19:00:00 +0100

    ajout du calcul de la construction d'une borne electrique

---
+++ ROIBorne.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from calcul_roi_v2 import *
+import datetime
+
+#Coût moyen du raccordement d'une borne de 22kw Wallbox
+def getPrixRaccordement():
+    return 2000
+
+#Coût moyen du d'une borne de 22kw Wallbox    
+def getPrixBorne():
+    return 1500
+
+"""
+Fonction permettant de calculer l'aide Advenir en prenant en hypothèse que 
+le client souhaite loué sa borne au public
+"""
+def aideAdvenir(prixRaccordement,prixBorne):
+    anneeCourante = datetime.datetime.now().year
+    if anneeCourante == 2020 or anneeCourante == 2021:
+        aide = 0.6*(prixRaccordement+prixBorne)
+    elif anneeCourante == 2022 or anneeCourante == 2023:
+        aide = 0.2*(prixRaccordement+prixBorne)
+    if aide > 2700:
+        aide = 2700
+    return aide
+    
+    
+"""
+Fonction calculant le coût de construction d'une borne en s'appuyant sur les données
+des Wallbox 22kw.
+Prend en entrée :  - aidesBornes = list() des aides
+                   - prixRaccordement = int() prix du raccordement d'une borne au
+                     réseau électrique
+                   - prixBorne = int() prix d'une borne électrique 
+"""
+def CoutConstructionBorne(aidesBornes,prixRaccordement,prixBorne):
+    return prixRaccordement+prixBorne-sum(aidesBornes)
+    
+if __name__ == "__main__":
+	print(CoutConstructionBorne([aideAdvenir(getPrixBorne(),getPrixRaccordement())],getPrixRaccordement(),getPrixBorne()))
+
+
+
+    
+    
