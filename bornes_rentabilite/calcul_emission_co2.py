a68191e15d90edd94bba3ea293083eb1e35f3e38
unknown <alexandre.galas@grenoble-inp.fr>
2021-01-25 11:30:09 +0100

    Supression de print

--- bornes_rentabilite/calcul_emission_co2.py
+++ bornes_rentabilite/calcul_emission_co2.py
@@ -14,7 +14,7 @@ def calcul_baisse_emission(nb_voitures, nb_utilitaires, km, pourcentage):
 	vce, vct, vre, vrt, uce, uct, ure, urt = emission["voiture_citadin_elec"], emission["voiture_citadin_thermique"],emission["voiture_rural_elec"], emission["voiture_rural_thermique"], emission["utilitaire_citadin_elec"], emission["utilitaire_citadin_thermique"], emission["utilitaire_rural_elec"], emission["utilitaire_rural_thermique"]
 
 	# citadin + rural
-	baisse = (nb_voitures * km * (vct - vce)) + (nb_utilitaires * km * (uct - uce) * pourcentage     *0.01) \
-	+(nb_voitures * km * (vrt - vre)) + (nb_utilitaires * km * (urt - ure) * (1-pourcentage) *0.01)
+	baisse = ((nb_voitures * km * (vct - vce)) + (nb_utilitaires * km * (uct - uce))) * (pourcentage     *0.01) \
+	+((nb_voitures * km * (vrt - vre)) + (nb_utilitaires * km * (urt - ure))) * ((100-pourcentage) *0.01)
 
 	return baisse
