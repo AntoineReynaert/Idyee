7b7713d9ad8917354e1b3b0a5936e9d946aca1ec
unknown <alexandre.galas@grenoble-inp.fr>
2021-02-03 14:45:15 +0100

    ajout de la fonction getAnnualProd

--- photovoltaique/PVGISapi.py
+++ photovoltaique/PVGISapi.py
@@ -5,12 +5,17 @@ import requests, json
 def getProduction(latitude,longitude,inclinaison,orientation):
     url = "https://re.jrc.ec.europa.eu/api/PVcalc?lat=" + str(latitude) + "&lon=" + str(longitude) + "&peakpower=6&loss=16&outputformat=json&angle=" + str(inclinaison) + "&aspect=" + str(orientation)
     r = requests.get(url).json()
-    return r["outputs"]
+    return r
     
 def getMonthlyProd(jsonPVGIS):
     result = dict()
-    for elem in jsonPVGIS["monthly"]["fixed"]:
+    for elem in jsonPVGIS["outputs"]["monthly"]["fixed"]:
         result[elem["month"]]=elem["E_m"]
     return result
+    
+def getAnnualProd(jsonPVGIS):
+    return jsonPVGIS["outputs"]["totals"]["fixed"]["E_m"]
 
-print(getMonthlyProd(getProduction(45,8,20,-45)))
+jsonProd = getProduction(45,8,20,-45) 
+print(getMonthlyProd(jsonProd))
+print(getAnnualProd(jsonProd))
